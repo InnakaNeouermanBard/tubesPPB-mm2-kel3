d41b700979db16a6f67ad16ab92aafb13a97031d
nakabard <innakabard@gmail.com>
2024-06-20 21:21:20 +0700

    delete

--- kas_kontrakan/android/app/src/main/kotlin/com/example/kas_kontrakan/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.kas_kontrakan
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- kas_kontrakan/lib/API/flutter_api/read.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-$conn = mysqli_connect("localhost", "root", "", "kas_kontrakan");
-$query = mysqli_query($conn, "SELECT * from mahasiswa");
-$data = mysqli_fetch_all($query, MYSQLI_ASSOC);
-echo json_encode($data);
